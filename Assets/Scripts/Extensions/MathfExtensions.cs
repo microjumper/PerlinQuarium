683bc03b76b858e5fb500da1964ad7503908802c
microjumper <microjmpr@gmail.com>
2025-10-30 18:01:54 +0100

    Refactoring

--- Assets/Scripts/Extensions/MathfExtensions.cs
+++ Assets/Scripts/Extensions/MathfExtensions.cs
@@ -7,13 +7,13 @@ namespace Extensions
         /// <summary>
         /// Maps a value from one range to another, clamping the input value within the source range.
         /// </summary>
-        public static float Map(float value, float sourceLowerBound, float sourceUpperBound, float targetLowerBound, float targetUpperBound)
+        public static float Map(float value, float sourceMin, float sourceMax, float targetMin, float targetMax)
         {
             // Clamp the value to the source range
-            value = Mathf.Clamp(value, sourceLowerBound, sourceUpperBound);
+            value = Mathf.Clamp(value, sourceMin, sourceMax);
             
             // Perform linear mapping
-            return (value - sourceLowerBound) / (sourceUpperBound - sourceLowerBound) * (targetUpperBound - targetLowerBound) + targetLowerBound;
+            return (value - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
         }
     }
 }
